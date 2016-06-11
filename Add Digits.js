a2568f6575e970490cba9057e6cbb1356db35806
Hongbin Chen <hongbin1992@hotmail.com>
2016-06-11 12:32:53 -0400

    Create Add Digits.js

---
+++ Add Digits.js
@@ -0,0 +1,19 @@
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num ===0) return 0;
+    else if(num%9 ===0){
+        return 9;
+    }
+    else{
+        return num%9;
+    }
+};
